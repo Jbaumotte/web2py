d3ee0a39771716a3bf230c09821e4b04541e3743
Joao Baumotte <jbaumotte@gmail.com>
2013-09-24 16:21:36 -0300

    24/09/2013

--- heroku.sh
+++ heroku.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+read -p "Choose your message to commit" msg
 virtualenv venv --distribute
 source venv/bin/activate
 pip freeze > requirements.txt
-echo "web: python web2py.py -a 'felipe' -i 0.0.0.0 -p $PORT" > Procfile
+echo "web: python web2py.py -a 'felipe' -i 0.0.0.0 -p \$PORT" > Procfile
 git add .
 git add Procfile
-git commit -a -m "Submit"
+git commit -a -m "$msg"
 git push heroku master
 heroku addons:add heroku-postgresql:dev
 heroku scale web=1
